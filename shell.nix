203c1342256f3f3846fc19a631304d93760f6c84
Jakub Skokan <jakub.skokan@havefun.cz>
2021-09-21 09:18:34 +0200

    shell.nix: link used qt, gcc and gdb for easier access

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
 { pkgs ? (import <nixpkgs> {}) }:
 let
   f =
-    { stdenv, qt5Full, qtcreator, pkgconfig, qmake, x11, openssl, gdb }:
+    { stdenv, qt5Full, qtcreator, pkgconfig, qmake, x11, openssl, gdb, gcc }:
     stdenv.mkDerivation {
       name = "ZIMA-CAD-Parts";
       src = ./.;
       nativeBuildInputs = [ qt5Full qtcreator pkgconfig qmake x11 gdb ];
       buildInputs = [ openssl ];
       shellHook = ''
-        echo ${qt5Full}
+        mkdir -p nix-build
+        ln -sfn ${qt5Full} nix-build/qt
+        ln -sfn ${gdb} nix-build/gdb
+        ln -sfn ${gcc} nix-build/gcc
       '';
     };
 in pkgs.libsForQt5.callPackage f {}
