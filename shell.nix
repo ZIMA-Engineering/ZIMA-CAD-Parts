609e27efee054921091ee64ea647b357d41f8ba4
Jakub Skokan <jakub.skokan@havefun.cz>
2023-02-10 12:39:34 +0100

    shell.nix: update for nixpkgs 22.11

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
 { pkgs ? (import <nixpkgs> {}) }:
 let
   f =
-    { stdenv, qt5Full, qtcreator, pkgconfig, qmake, x11, openssl, gdb, gcc }:
-    stdenv.mkDerivation {
+    { stdenv, qt5, qtcreator, pkgconfig, qmake, xlibsWrapper, openssl, gdb, gcc }:
+    let
+      zcpQt = qt5.env "qt-zcp-${qt5.qtbase.version}" (with qt5; [
+        qtbase qtdeclarative qtdoc qtimageformats qtlocation qtquickcontrols qtquickcontrols2
+        qtsvg qttools qttranslations qtwebengine qtwebchannel qtx11extras
+      ]);
+    in stdenv.mkDerivation {
       name = "ZIMA-CAD-Parts";
       src = ./.;
-      nativeBuildInputs = [ qt5Full qtcreator pkgconfig qmake x11 gdb ];
+      nativeBuildInputs = [ zcpQt qtcreator pkgconfig qmake xlibsWrapper gdb ];
       buildInputs = [ openssl ];
       shellHook = ''
         mkdir -p nix-build
-        ln -sfn ${qt5Full} nix-build/qt
+        ln -sfn ${zcpQt} nix-build/qt
         ln -sfn ${gdb} nix-build/gdb
         ln -sfn ${gcc} nix-build/gcc
       '';
